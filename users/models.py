0397e2e4ab87e594865e1967be3927491f86fedf
markstanik <50532441+markstanik@users.noreply.github.com>
2021-03-20 16:24:23 -0400

    got the server running

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ExtendedUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    firstname = models.CharField(max_length=50, verbose_name="First Name:")
+    lastname = models.CharField(max_length=50, verbose_name="Last Name:")
+    description = models.TextField(default="This user has not provided an about me.")
+    image = models.TextField(default="http://via.placeholder.com/150")
+
+
+class Organization(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    organization_name = models.CharField(max_length=55, verbose_name="Organization name:")
+    description = models.TextField(default="This organization has not provided a description.")
+    image = models.TextField(default="http://via.placeholder.com/150")
+    
--- web_project/settings.py
+++ web_project/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'root',
-        'USER': 'zinc-presence-308217:us-central1:unisearch',
+        'NAME': 'data',
+        'USER': 'root',
         'PASSWORD': 'designdevelop2021',
         'HOST': '104.155.128.146',
         'PORT': '3306',
