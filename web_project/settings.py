390580c4b5a270bdc8d3804ee1ad384bb8f6f726
markstanik <50532441+markstanik@users.noreply.github.com>
2021-03-20 15:25:01 -0400

    created users in Django

--- web_project/settings.py
+++ web_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'root',
+        'USER': 'zinc-presence-308217:us-central1:unisearch',
+        'PASSWORD': 'designdevelop2021',
+        'HOST': '104.155.128.146',
+        'PORT': '3306',
     }
 }
 
